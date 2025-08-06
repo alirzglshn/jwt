fa2eeedf888018c08cf2e87fd58235b065a835b8
Alireza <golshandeveloper@gmail.com>
2025-08-06 19:59:57 +0330

    updated load_dotenv() in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,8 @@ from datetime import timedelta
 from dotenv import load_dotenv
 
 import os
-load_dotenv()
+
+load_dotenv('/app/.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
